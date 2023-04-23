d9da6600305046c8445221353f104179b04229d7
Ishan Shrestha <ishan.shrestha356@gmail.com>
2023-04-23 15:51:23 +0545

    feat: add tests for index page and list view

--- news/tests.py
+++ news/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.utils import timezone
 from news.models import Content
+from django.urls import reverse
 # Create your tests here.
 
 class NewsModelTest(TestCase):
@@ -16,4 +17,15 @@ class NewsModelTest(TestCase):
     def test_content_content(self):
         self.assertEqual(self.content.title, "Learn Django")
         self.assertEqual(self.content.description, "Learning the latest version of django")
-        
+    
+    def test_index_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+    
+    def test_index_page_correct_template_use(self):
+        response = self.client.get(reverse("news:index"))
+        self.assertTemplateUsed(response, "index.html")
+    
+    def test_index_list_contains(self):
+        response = self.client.get(reverse("news:index"))
+        self.assertContains(response, "Learn Django")
